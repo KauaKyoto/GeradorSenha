b2b0f631bdb6207e7de52d3efcff2b8be452d3f5
Kaua Kyoto <kauaantonioetec@gmail.com>
2024-09-25 17:40:04 +0000

    save2

--- geradorSenhas/App.js
+++ geradorSenhas/App.js
@@ -1,10 +1,12 @@
 import { useState } from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity, Model } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity, Modal } from 'react-native';
+import { ModalPassword } from './src/modal/index'
  
 let charset = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
  
 export default function App() {
   const [senhaGerada, setSenhaGerada] = useState("")
+  const [modalVisible, setModalVisible] = useState(false)
  
   function gerarSenha(){
    
@@ -14,8 +16,8 @@ export default function App() {
       senha += charset.charAt(Math.floor(Math.random() * n));
     }
  
-    setSenhaGerada(senha)
- 
+    setSenhaGerada(senha);
+    setModalVisible(true);
   }
  
  
@@ -31,7 +33,10 @@ export default function App() {
       <TouchableOpacity style={styles.button} onPress={gerarSenha}>
         <Text style={styles.textButton}> Gerar Senha </Text>
       </TouchableOpacity>
- 
+
+      <Modal visible={modalVisible} animationType="fade" transparent={true}>
+      <ModalPassword senha={senhaGerada} handleClose={ () => setModalVisible(false) } />
+      </Modal>
      
       <Text style={styles.senha}> {senhaGerada} </Text>
     </View>
